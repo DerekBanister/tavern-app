5233dc656d581488d19973601a573e2b2340d579
unknown <ptownderek@yahoo.com>
2022-05-26 20:53:15 -0700

    great start, first api call is bringing in members, 2nd call is pulling specific data on each member. limiting to 5 calls to not hit 429 responses

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 
+
 async function getMemberList() {
     //the polar badlands prefix to the temple api url is a cors bypass.
     const response = await fetch(`https://polar-badlands-45238.herokuapp.com/https://templeosrs.com/api/group_info.php?id=1061`, {
@@ -16,30 +17,55 @@ async function getMemberList() {
     //return the list
     return ccList;
 }
-
+// `https://polar-badlands-45238.herokuapp.com/https://templeosrs.com/api/player_info.php?player=${singleMember}`
 async function getSinglePlayer() {
 
     let ccList = getMemberList();
-    ccList.then(function (result) {
-
-        console.log(result);
-        // result.length
-        //for testing, using 5 members to not get 429 response from temple
-        for (i = 0; i < 5; i++) {
-            let singleMember = result[i];
-            // console.log(singleMember);
-            // const response = await fetch(`https://polar-badlands-45238.herokuapp.com/https://templeosrs.com/api/player_info.php?player=${singleMember}`, {
-            //     method: "GET",
-            //     headers: {
-            //         "Content-Type": "application/json",
-            //         "Accept": "application/json"
-            //     },
-            // });
-            console.log(singleMember);
-            // let member = await response.json();
-            // console.log(member);
+    ccList.then(async function (list) {
+        // console.log(list);
+        for (let i = 0; i < 5; i++) {
+            let singleMember = list[i];
+            const response = await fetch(`https://polar-badlands-45238.herokuapp.com/https://templeosrs.com/api/player_info.php?player=${singleMember}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+            })
+            let ccMembers = await response.json();
+            console.log(ccMembers);
         }
-    })
+
+
+    }
+    )
 }
 
 getSinglePlayer();
+
+
+
+// async function getSinglePlayer() {
+
+//     let ccList = getMemberList();
+//     ccList.then(function (list) {
+//         // console.log(list);
+
+//         let promises = [];
+//         //for testing, using 5 members to not get 429 response from temple // list.length
+//         for (i = 0; i < 5; i++) {
+//             let singleMember = list[i];
+//             // console.log(singleMember);
+//             promises.push(fetch(`https://polar-badlands-45238.herokuapp.com/https://templeosrs.com/api/player_info.php?player=${singleMember}`, {
+//                 method: "GET",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                     "Accept": "application/json"
+//                 },
+//             }));
+//             Promise.all(promises).then(function (data) {
+//                 console.log(data);
+//             })
+//         }
+//     })
+// }
