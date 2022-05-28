78b5b543faaefb0a1f7278d83febbc8df06fbffb
unknown <ptownderek@yahoo.com>
2022-05-27 18:43:56 -0700

    data on page, date formatted. big steps

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-
+let listEl = document.querySelector(".idea");
 
 async function getMemberList() {
     //the polar badlands prefix to the temple api url is a cors bypass.
@@ -19,12 +19,15 @@ async function getMemberList() {
 }
 // `https://polar-badlands-45238.herokuapp.com/https://templeosrs.com/api/player_info.php?player=${singleMember}`
 async function getSinglePlayer() {
-
+    //get cc member list from the function above
     let ccList = getMemberList();
+
     ccList.then(async function (list) {
-        // console.log(list);
+        // list.length, for now using 5 so I dont hit 429's
+        //looping through member array to get a specific member, then put their username in another api call to retreive player specific data
         for (let i = 0; i < 5; i++) {
             let singleMember = list[i];
+            //api call for single members from member list
             const response = await fetch(`https://polar-badlands-45238.herokuapp.com/https://templeosrs.com/api/player_info.php?player=${singleMember}`, {
                 method: "GET",
                 headers: {
@@ -32,40 +35,31 @@ async function getSinglePlayer() {
                     "Accept": "application/json"
                 },
             })
+            //this var is a single cc members data
             let ccMembers = await response.json();
-            console.log(ccMembers);
+            //parsing out data
+            let individual = ccMembers.data;
+
+            //individual username
+            let username = individual.Username;
+
+            //last time the user was checked at templeosrs
+            let lastChecked = individual["Last checked"];
+            //cutting off useless timestamp on lastChecked
+            let formatted = lastChecked.split(" ")[0];
+            //initiatng empty str to put reversed date into
+
+            //decrementing forloop to reverse date
+
+
+            //creating a element for each data point
+            let li1 = document.createElement("li");
+            //one gets username
+            li1.textContent = username + " |||| " + formatted;
+            //other gets date
+            //append both to page
+            listEl.appendChild(li1);
         }
-
-
-    }
-    )
+    })
 }
-
-getSinglePlayer();
-
-
-
-// async function getSinglePlayer() {
-
-//     let ccList = getMemberList();
-//     ccList.then(function (list) {
-//         // console.log(list);
-
-//         let promises = [];
-//         //for testing, using 5 members to not get 429 response from temple // list.length
-//         for (i = 0; i < 5; i++) {
-//             let singleMember = list[i];
-//             // console.log(singleMember);
-//             promises.push(fetch(`https://polar-badlands-45238.herokuapp.com/https://templeosrs.com/api/player_info.php?player=${singleMember}`, {
-//                 method: "GET",
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                     "Accept": "application/json"
-//                 },
-//             }));
-//             Promise.all(promises).then(function (data) {
-//                 console.log(data);
-//             })
-//         }
-//     })
-// }
+getSinglePlayer()
