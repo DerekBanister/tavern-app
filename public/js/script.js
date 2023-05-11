49a82b1e29ed1d82d3d90587655cb82f91a70caa
unknown <ptownderek@yahoo.com>
2023-05-11 12:00:31 -0700

    adding logic to sort clan members while loading by date (oldest to newest)

--- public/js/script.js
+++ public/js/script.js
@@ -25,18 +25,16 @@ async function getMemberList() {
 // `https://polar-badlands-45238.herokuapp.com/https://templeosrs.com/api/player_info.php?player=${singleMember}`
 async function getSinglePlayer() {
     //get cc member list from the function above
-
     let ccList = getMemberList();
 
     ccList.then(async function (list) {
         // list.length, for now using 5 so I dont hit 429's
         //looping through member array to get a specific member, then put their username in another api call to retreive player specific data
 
+        // Initialize an array to hold the user data
+        let users = [];
+
         for (let i = 0; i < list.length; i++) {
-
-            //sleep for js
-            await new Promise(r => setTimeout(r, 500));
-
             let singleMember = list[i];
 
             //api call for single members from member list
@@ -48,52 +46,54 @@ async function getSinglePlayer() {
                 },
             })
 
-
             //this var is a single cc members data
             let ccMembers = await response.json();
+
             //parsing out data
-            // console.log(ccMembers);
             let individual = ccMembers.data;
-            console.log(individual);
-            //individual username
             let username = individual.Username;
-
-            //last time the user was checked at templeosrs
             let lastChecked = individual["Last changed"];
-            //last active unix time
             let unixDate = Math.floor(new Date(lastChecked).getTime())
-            //30 days previous from active time
             let past = unixDate - 2592000 * 1000;
-
-            // console.log(unixDate);
-            // console.log(past);
             let current = Date.now();
-
-            //cutting off useless timestamp on lastChecked
             let formatted = lastChecked.split(" ")[0];
-            //Split date on the -  (2022-05-27 will be 2022 at index 0, 05 at index 2, etc..)
             let newStr = formatted.split("-");
-            //reverse date format
             let lastActive = newStr[1] + '-' + newStr[2] + '-' + newStr[0];
-
-            //current - 30 days
             let thousand = current - past / 1000;
 
             if (thousand >= unixDate) {
-                //creating a element for each data point
-                let li1 = document.createElement("li");
+                // Add user data to the users array
+                users.push({ username, lastActive });
 
-                // put username and last active date in the ol
-                li1.textContent = username + " | Last Active: " + lastActive;
+                // Sort the users array by lastActive date
+                users.sort((a, b) => new Date(a.lastActive) - new Date(b.lastActive));
 
-                //append both to page
-                listEl.appendChild(li1);
+                // Display the sorted list of users
+                displayUsers(users);
+
+                // Sleep for 500ms before loading the next user
+                await new Promise(r => setTimeout(r, 500));
             }
-
         }
-
     })
 }
-getSinglePlayer();
 
+function displayUsers(users) {
+    // Clear the existing list
+    listEl.innerHTML = "";
+
+    // Display the list of users in the sorted order
+    users.forEach((user) => {
+        //creating a element for each data point
+        let li1 = document.createElement("li");
+
+        // put username and last active date in the ol
+        li1.textContent = user.username + " | Last Active: " + user.lastActive;
+
+        //append both to page
+        listEl.appendChild(li1);
+    });
+}
+
+getSinglePlayer();
 // store unix times in array, then display in order
